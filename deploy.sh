89e31632e4de2d3d5aa5b71d068aff606570b1af
shengj <shengj@rd.netease.com>
2018-07-14 10:37:10 +0800

    rebuilding site 2018年07月14日 10:37:10

--- deploy.sh
+++ deploy.sh
@@ -22,3 +22,16 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
